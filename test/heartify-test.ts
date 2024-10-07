ae7dbc63aaafa99f37aefe1785bf897d880fe052
Draimonox <Draimonox@gmail.com>
2024-10-07 19:41:54 -0400

    test no errors

--- test/heartify-test.ts
+++ test/heartify-test.ts
@@ -1,16 +1,20 @@
 import { ethers } from "hardhat";
 import { expect } from "chai";
 import { Heartify } from "../typechain-types"; // Adjust the path as necessary
-import { Signer } from "ethers";
+import { BigNumber, Signer } from "ethers";
 
 describe("Heartify NFT Contract", function () {
-  let heartify: Heartify; // Heartify contract instance
+  let heartify: Heartify;
   let owner: Signer;
   let artist: Signer;
+  let buyer: Signer;
+  let dev: Signer;
+  const mintingFee = ethers.utils.parseEther("15"); // 15 MATIC
 
-  before(async function () {
+  beforeEach(async function () {
     const HeartifyFactory = await ethers.getContractFactory("Heartify");
-    [owner, artist] = await ethers.getSigners();
+    [owner, artist, buyer, dev] =
+      (await ethers.getSigners()) as unknown as Signer[];
 
     heartify = (await HeartifyFactory.deploy(
       await owner.getAddress()
@@ -18,44 +22,145 @@ describe("Heartify NFT Contract", function () {
     await heartify.deployed();
   });
 
-  it("Should mint an NFT", async function () {
+  it("should set the correct minting fee", async function () {
+    expect(await heartify.mintingFee()).to.equal(mintingFee);
+  });
+
+  it("should mint an NFT", async function () {
     await heartify
       .connect(artist)
       .safeMint(await artist.getAddress(), { value: mintingFee });
-
-    const balance: BigNumber = await heartify.balanceOf(
+    const tokenId = 1;
+    expect(await heartify.ownerOf(tokenId)).to.equal(await artist.getAddress());
+    expect(await heartify.getArtist(tokenId)).to.equal(
       await artist.getAddress()
     );
-    expect(balance).to.equal(1); // Check that the artist has 1 NFT
   });
 
-  it("Should list an NFT for sale", async function () {
-    await heartify.connect(artist).listNFT(1, tokenPrice);
-    const price: BigNumber = await heartify.getTokenPrice(1);
-    expect(price).to.equal(tokenPrice); // Verify the listed price
+  it("should not mint if insufficient funds are sent", async function () {
+    await expect(
+      heartify.connect(artist).safeMint(await artist.getAddress(), {
+        value: ethers.utils.parseEther("10"),
+      }) // Less than 15
+    ).to.be.revertedWith("Insufficient funds to mint");
   });
 
-  it("Should allow a buyer to purchase the listed NFT", async function () {
-    await heartify.connect(buyer).buyNFT(1, { value: tokenPrice });
-
-    const newOwner: string = await heartify.ownerOf(1);
-    expect(newOwner).to.equal(await buyer.getAddress()); // Check the new owner of the NFT
+  it("should allow batch minting of NFTs", async function () {
+    await heartify
+      .connect(artist)
+      .batchMint(await artist.getAddress(), 3, { value: mintingFee.mul(3) });
+    expect(await heartify.balanceOf(await artist.getAddress())).to.equal(3);
   });
 
-  it("Should refund excess ETH to buyer", async function () {
-    const excessAmount: BigNumber = ethers.utils.parseEther("7"); // Buyer sends 7, needs 2 refunded
-    const balanceBefore: BigNumber = await buyer.getBalance();
-
-    const tx = await heartify.connect(buyer).buyNFT(1, { value: excessAmount });
-    const receipt = await tx.wait();
-    const gasCost: BigNumber = receipt.gasUsed.mul(receipt.effectiveGasPrice);
-    const balanceAfter: BigNumber = await buyer.getBalance();
-
-    const allowedMargin: BigNumber = ethers.utils.parseEther("0.1"); // Allow a small margin for gas cost
-
-    expect(balanceAfter.add(gasCost).add(tokenPrice)).to.be.closeTo(
-      balanceBefore,
-      allowedMargin // Use the defined margin for comparison
+  it("should list an NFT for sale", async function () {
+    await heartify
+      .connect(artist)
+      .safeMint(await artist.getAddress(), { value: mintingFee });
+    await heartify.connect(artist).listNFT(1, ethers.utils.parseEther("100"));
+    expect(await heartify.getTokenPrice(1)).to.equal(
+      ethers.utils.parseEther("100")
     );
+  });
+
+  it("should not allow listing an NFT that the user does not own", async function () {
+    await heartify
+      .connect(artist)
+      .safeMint(await artist.getAddress(), { value: mintingFee });
+    await expect(
+      heartify.connect(buyer).listNFT(1, ethers.utils.parseEther("100"))
+    ).to.be.revertedWith("You do not own this token");
+  });
+
+  it("should allow buying an NFT", async function () {
+    await heartify
+      .connect(artist)
+      .safeMint(await artist.getAddress(), { value: mintingFee });
+    await heartify.connect(artist).listNFT(1, ethers.utils.parseEther("100"));
+    await heartify
+      .connect(buyer)
+      .buyNFT(1, { value: ethers.utils.parseEther("100") });
+
+    expect(await heartify.ownerOf(1)).to.equal(await buyer.getAddress());
+  });
+
+  it("should calculate royalties correctly on purchase", async function () {
+    await heartify
+      .connect(artist)
+      .safeMint(await artist.getAddress(), { value: mintingFee });
+    await heartify.connect(artist).listNFT(1, ethers.utils.parseEther("100"));
+
+    const initialDevBalance = await ethers.provider.getBalance(
+      await dev.getAddress()
+    );
+    const initialArtistBalance = await ethers.provider.getBalance(
+      await artist.getAddress()
+    );
+
+    await heartify
+      .connect(buyer)
+      .buyNFT(1, { value: ethers.utils.parseEther("100") });
+
+    const finalDevBalance = await ethers.provider.getBalance(
+      await dev.getAddress()
+    );
+    const finalArtistBalance = await ethers.provider.getBalance(
+      await artist.getAddress()
+    );
+
+    const artistRoyalty = (100 * 300) / 10000; // 3% of 100
+    const devRoyalty = (100 * 300) / 10000; // 3% of 100
+
+    expect(finalDevBalance - initialDevBalance).to.equal(
+      ethers.utils.parseEther(devRoyalty.toString())
+    );
+    expect(finalArtistBalance - initialArtistBalance).to.equal(
+      ethers.utils.parseEther(artistRoyalty.toString())
+    );
+  });
+
+  it("should not allow buying an NFT that is not listed", async function () {
+    await heartify
+      .connect(artist)
+      .safeMint(await artist.getAddress(), { value: mintingFee });
+    await expect(
+      heartify
+        .connect(buyer)
+        .buyNFT(1, { value: ethers.utils.parseEther("100") })
+    ).to.be.revertedWith("Token is not listed for sale");
+  });
+
+  it("should allow the owner to withdraw funds", async function () {
+    await heartify
+      .connect(artist)
+      .safeMint(await artist.getAddress(), { value: mintingFee });
+    await heartify.connect(artist).listNFT(1, ethers.utils.parseEther("100"));
+    await heartify
+      .connect(buyer)
+      .buyNFT(1, { value: ethers.utils.parseEther("100") });
+
+    const initialDevBalance = await ethers.provider.getBalance(
+      await dev.getAddress()
+    );
+    await heartify.withdrawFunds();
+    const finalDevBalance = await ethers.provider.getBalance(
+      await dev.getAddress()
+    );
+
+    expect(finalDevBalance).to.be.gt(initialDevBalance); // Check if funds have been withdrawn
+  });
+
+  it("should pause and unpause the contract", async function () {
+    await heartify.pause();
+    await expect(
+      heartify
+        .connect(artist)
+        .safeMint(await artist.getAddress(), { value: mintingFee })
+    ).to.be.revertedWith("Pausable: paused");
+
+    await heartify.unpause();
+    await heartify
+      .connect(artist)
+      .safeMint(await artist.getAddress(), { value: mintingFee });
+    expect(await heartify.ownerOf(1)).to.equal(await artist.getAddress());
   });
 });
